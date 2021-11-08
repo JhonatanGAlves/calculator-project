c27f9dfe8dd8a4f37879ebb8192e5dee5093289f
JhonatanGAlves <jhonatangalves96@gmail.com>
2021-11-08 10:29:49 -0300

    First commit/component Calculator created

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css';
+import Calculator from './main/Calculator';
+
+const App = () => {
+  return (
+    <div>
+      <Calculator />
+    </div>
+  )
+}
+
+export default App
+
---
+++ src/main/Calculator.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Calculator.css'
+
+const Calculator = () => {
+  return (
+    <div className="container">
+      <h1>Calculadora</h1>
+      <div className="calculator">
+        
+      </div>
+    </div>
+  )
+}
+
+export default Calculator
